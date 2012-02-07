72a72d01caff8175d3536a1b3c145878fb0f4db3
Greg Lazarev <russianbandit@gmail.com>
2012-02-06 21:57:50 -0800

    streaming to a haml template with css

--- world.rb
+++ world.rb
@@ -2,17 +2,6 @@ require 'goliath'
 require 'tilt'
 
 class World < Goliath::API
-	# render templated files from ./views
-	#include Goliath::Rack::Templates
-	# use Rack::Static,
-	# 	:urls => ["/index.html"],
-	# 	:root => Goliath::Application.app_path("public")
-
-	# render static files from ./public
-	#use(Rack::Static,
-	#	:root => Goliath::Application.app_path("public"),
-  # :urls => ["/favicon.ico", '/stylesheets', '/javascripts', '/images'])
-
   def response(env)
     EM.add_periodic_timer(1) do
     	# must have two \n at the end
@@ -23,12 +12,19 @@ class World < Goliath::API
   end
 end
 
+class Routes < Goliath::API
+	# render templated files from ./views
+	include Goliath::Rack::Templates
+	
+	# render static files from ./public
+	use(Rack::Static,
+		:root => Goliath::Application.app_path('public'),
+		:urls => ['/stylesheets'])
+		# :urls => ['/favicon.ico', '/stylesheets', '/javascripts', '/images'])
 
-class SSE < Goliath::API
-	use Rack::Static,
-		:urls => ["/index.html"],
-		:root => Goliath::Application.app_path("public")
-	get "/events" do
-		run World.new
-	end	
+	get '/world', World
+	
+	def response(env)
+		[200, {}, haml(:index)]
+	end
 end
