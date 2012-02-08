a6cd6bf65622d83f0e7af6bb8eb2f4fa349dfddd
Greg Lazarev <russianbandit@gmail.com>
2012-02-07 21:14:21 -0800

    added css and cleaned up js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-## Gotchas
+# Running
+To run the app:
+    ruby world.rb
+It will be accessible on port 9000
+
+# Gotchas
 The data (string) in ```stream_send``` method must end with two '\n' characters.
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,24 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var source = new EventSource('/world');
 
-  // new connection opened callback
   source.addEventListener('open', function(e) {
     console.log('connection opened');
   }, false);
 
-  // subscribe to unnamed messages
   source.onmessage = function(e) {
     console.log(e);
-    document.getElementById('world').innerHTML = e.data;
+    $('#world').html(e.data);
   };
 
-  // listen for signup events
-  source.addEventListener('signup', function(e) {
-    console.log(e);
-    document.body.innerHTML += e.data + '<br />';
-  }, false);
-
-  // connection closed callback
   source.addEventListener('error', function(e) {
     if (e.eventPhase == EventSource.CLOSED) {
       console.log('connection closed');
