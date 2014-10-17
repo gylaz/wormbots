2c6a2d77d0834ae0845ecc9817b7c3ec60746bd4
Greg Lazarev <greg@thoughtbot.com>
2014-10-17 14:09:47 -0700

    Fix config

--- hound_test.rb
+++ hound_test.rb
@@ -4,44 +4,4 @@ blah = 'hello world'
 { :one => blah }
 { :two => "three" }
 
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
 puts 'I say this: ' + blah
-puts 'I say this: ' + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
-puts "I say this: " + blah
